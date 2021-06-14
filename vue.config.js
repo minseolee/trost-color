81f4561165f38038b544eca27c2cb647ee2f97eb
minseolee <mmmm@ajou.ac.kr>
2021-06-14 23:16:03 +0900

    설정파일 다시 수정

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // publicPath: '../../../../dist/promotion/color/',
-  // outputDir: 'docs'
-  outputDir: '../../../../dist/promotion/color/',
-  devServer: {
-    compress: true,
-    disableHostCheck: true,
-  },
-}
